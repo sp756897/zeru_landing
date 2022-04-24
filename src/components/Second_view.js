5052d125e91abdaa659bac4103b9b11d5d633cd1
sp756897 <akshaysp2090@gmail.com>
2022-04-24 12:43:48 +0530

    second

---
+++ src/components/Second_view.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Second_View extends Component {
+    render() {
+        return (
+            <div >
+                <h1> Second_View </h1>
+            </div>
+        )
+    }
+}
+
+export default Second_View
